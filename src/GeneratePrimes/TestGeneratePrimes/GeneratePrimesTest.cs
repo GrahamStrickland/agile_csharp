dd8fda31c937a419ea94068ab1ca18fc1cea1a60
Graham Strickland <grahamhstrickland@gmail.com>
2024-02-10 08:18:34 +0200

    Add GeneratePrimes solution

---
+++ src/GeneratePrimes/TestGeneratePrimes/GeneratePrimesTest.cs
@@ -0,0 +1,11 @@
+namespace TestGeneratePrimes;
+
+[TestFixture]
+public class GeneratePrimesTest
+{
+    [Test]
+    public void TestPrimes()
+    {
+        int[] nullArray = GeneratePrimeNumbers(0);
+    }
+}
